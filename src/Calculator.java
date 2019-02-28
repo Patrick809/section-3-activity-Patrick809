0f6d2eedd1e78ad7a55f2ed0bcaa7cf866548b90
patrick.tena <patrick.tena@e3civichigh.com>
2019-02-28 11:23:55 -0800

    Calculator java

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,37 +3,46 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
 
+
         double num1;
         double num2;
-        double result=0;
+        double result = 0;
         int whichCalculaion;
 
         Scanner input = new Scanner(System.in);
+        int agian;
+        do {
+            System.out.println("Choose a operation. Press 1 to add, 2 to subtract, 3 to multiply, and 4 to divied");
 
-        System.out.println("Choose a task. Press 1 to add, 2 to subtract, 3 to multiply, and 4 to divied");
+            whichCalculaion = input.nextInt();
 
-        whichCalculaion = input.nextInt();
+            System.out.println("Please enter the first number.");
+            num1 = input.nextDouble();
 
-        System.out.println("Please enter the first number.");
-        num1 = input.nextDouble();
+            System.out.println("Please enter the second number.");
+            num2 = input.nextDouble();
 
-        System.out.println("Please enter the second number.");
-        num2 = input.nextDouble();
+            if (whichCalculaion == 1) {
+                result = num1 + num2;
+            } else if (whichCalculaion == 2) {
+                result = num1 - num2;
+            } else if (whichCalculaion == 3) {
+                result = num1 * num2;
+            } else if (whichCalculaion == 4) {
+                result = num1 / num2;
+            } else {
+                System.out.println("That is not a proper operation.");
+            }
+            System.out.println("The answer is...");
+            System.out.println(result);
+            System.out.println("Type 1 if you would you like to try another operation?");
+            System.out.println("Type 2 to leave calculator");
 
-        if (whichCalculaion == 1){
-            result = num1 + num2;
-        }
-        if (whichCalculaion == 2){
-            result = num1 - num2;
-        }
-        if (whichCalculaion == 3){
-            result = num1 * num2;
-        }
-        if (whichCalculaion == 4){
-            result = num1 / num2;
-        }
-        System.out.println("The answer is...");
-        System.out.println(result);
+            agian = input.nextInt();
 
+        } while (agian == 1);
+//        {
+//            System.out.println("would you like to try another operation?");
+//        }
     }
 }
